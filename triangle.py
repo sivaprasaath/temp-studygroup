3563bf317ee2950371b47dc659d646cd6e963e8b
siva <sivaprasaath.s@gmail.com>
2020-04-17 13:10:39 +0530

    oop example

---
+++ triangle.py
@@ -0,0 +1,15 @@
+class triangle(object):
+    num_of_sides=3
+    def __init__(self,angle1,angle2,angle3):
+        self.angle1=angle1
+        self.angle2=angle2
+        self.angle3=angle3
+    def check_angles(self):
+        if self.angle1 +self.angle2 +self.angle3==180:
+            return True
+        else:
+            return False
+
+my_triangle=triangle(30,60,90)
+print(my_triangle.num_of_sides)
+print(my_triangle.check_angles())
